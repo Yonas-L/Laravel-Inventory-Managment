671d8842f26c4fc62fa8039518a35d2adecea3b9
Yonas legesse <Yonaslegesse19@gmail.com>
2023-09-26 00:44:28 +0300

    write test for remember token

--- tests/Feature/AuthenticationUITest.php
+++ tests/Feature/AuthenticationUITest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AuthenticationViewTest extends TestCase
+class AuthenticationUITest extends TestCase
 {
 
     public function test_login_form_renders_correctly(): void
--- tests/Feature/UserLoginTest.php
+++ tests/Feature/UserLoginTest.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Database\Factories\UserFactory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
@@ -18,15 +19,50 @@ class UserLoginTest extends TestCase
     public function test_user_can_login_only_using_email_and_password()
     {
         // creating the user
-       $user= UserFactory::new()->create();
-        $response = $this->post('login',[$user]);
+        $user = UserFactory::new()->create([
+            'password' => bcrypt($password = '434344434344'),
+        ]);
+        // Try to log in
+        $response = $this->post('login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
         $response->assertRedirectToRoute('home');
+        $this->assertAuthenticatedAs($user);
     }
+
 
     public function test_user_cannot_see_login_form_if_authenticated()
     {
         $user = UserFactory::new()->create();
         $response = $this->actingAs($user)->get('login');
         $response->assertRedirectToRoute('home');
+    }
+    public function test_user_cannot_see_dashboard_if_not_logged_in()
+    {
+        $response = $this->get('/');
+        $response->assertRedirect('login');
+    }
+    public function test_remember_me_checkbox_works()
+    {
+        $user = UserFactory::new()->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+
+        $response->assertRedirectToRoute('home');
+        $this->assertAuthenticatedAs($user);
+
+        // cookie assertion goes here
+        $response->assertCookie(Auth::guard()->getRecallerName(), vsprintf('%s|%s|%s', [
+            $user->id,
+            $user->getRememberToken(),
+            $user->password,
+        ]));
     }
 }
